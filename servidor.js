685ec96facffbe070c416a8f0de28961e4e18cff
JuanchoStock <juanchostock@gmail.com>
2025-04-07 14:56:45 -0300

    Se corrigio error de CORS

--- servidor.js
+++ servidor.js
@@ -1,14 +1,16 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const port = 3000;
 
-//Escucha la petición del tipo GET en la ruta que le indicamos.
+app.use(cors());
+
 app.get('/saludo/:nombre', (req, res) => {
-    const nombre = req.params.nombre;
-    res.send(`Hola, ${nombre}!`);
+  const nombre = req.params.nombre;
+  res.send(`Hola, ${nombre}!`);
 });
 
-//Estará escuchando en el puerto 3000, que definimos en el principio.
 app.listen(port, () => {
-    console.log(`Servidor escuchando en http://localhost:${port}`);
+  console.log(`Servidor escuchando en http://localhost:${port}`);
 });
